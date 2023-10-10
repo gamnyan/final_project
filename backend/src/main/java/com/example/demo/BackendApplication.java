b8be5046a488439b87cfaeb7d40df5e7c49985fe
Yeonghun Kim <onlysungkim@gmail.comgit config --global user.email onlysungkim@gmail.com>
2023-10-10 14:07:49 +0900

    re

--- backend/src/main/java/com/example/demo/BackendApplication.java
+++ backend/src/main/java/com/example/demo/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FunApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FunApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
