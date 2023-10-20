b1e60822c33770c617924ef441f05a4346708669
drang <drang2536@gmail.com>
2023-10-20 13:07:03 +0900

    2023.10-20sh

--- backend/src/main/java/com/avado/backend/model/Comment.java
+++ backend/src/main/java/com/avado/backend/model/Comment.java
@@ -1,13 +1,23 @@
 package com.avado.backend.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -32,6 +42,11 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id")
     private Article article;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "communityid")
+	@JsonIgnoreProperties("community")
+	private Community community;
 
 
     @Builder
