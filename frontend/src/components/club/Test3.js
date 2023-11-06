646f4075185d887c4cab82091335b38aabc6fed5
Woomunsik <wms170513@gmail.com>
2023-11-06 12:43:07 +0900

    sj_1106

--- frontend/src/components/club/Test3.js
+++ frontend/src/components/club/Test3.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from "react";
+import ClubItemNavigation from "../Layout/ClubItemNavigation";
+import { Col } from "react-bootstrap";
 
 const Test3 = () => {
   return (
     <Fragment>
-      <h2>test 3333</h2>
+      <Col xs={12}>
+        <ClubItemNavigation />
+        <h2>test 3333</h2>
+      </Col>
     </Fragment>
   );
 };
